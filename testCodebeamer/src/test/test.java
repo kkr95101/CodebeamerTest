33349b098fa4b3ddae68082b2316d88954a9f939
Administrator <Administrator@cb.digiteki.com>
2024-05-17 16:20:52 +0900

    testeee

--- testCodebeamer/src/test/test.java
+++ testCodebeamer/src/test/test.java
@@ -3,5 +3,8 @@ package test;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("eee");
+		System.out.println("ddd");
+		System.out.println("ddd");
+		System.out.println("ddd");
 	}
 }
